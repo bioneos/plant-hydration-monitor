51360f97d56319372308ec01b92aa43f16b8c222
Steven Davis <sgdavis@bioneos.com>
2024-01-18 14:21:58 -0600

    Improved deep sleep code and documentation

--- hygrometer-esp8266-iot-node/hygrometer-esp8266-iot-node.ino
+++ hygrometer-esp8266-iot-node/hygrometer-esp8266-iot-node.ino
@@ -1,20 +1,21 @@
 #include <ESP8266WiFi.h>
 
 // NOTE: Change these values to the WiFi values for your personal WiFi
-const char SSID[] =  "12AshCt"; // Your current WiFi network SSID (can be hidden)
-const char PASS[] =  "3196215249"; // Your current WiFi network password
+const char SSID[] =  "SSID"; // Your current WiFi network SSID (can be hidden)
+const char PASS[] =  "XXXXXXXXXX"; // Your current WiFi network password
 const char *SERVER = "192.168.0.0"; // Your IP address on the WiFi network
 const int SERVER_PORT = 3000;
-const int SLEEP_MS = 5 * 60e6;
 
 // Main loop process:
 //   1) Wake from sleep
 //   2) Read from the sensor NUM_READS times, READ_DELAY_APART second apart
 //   3) Average readings for a value and POST back to the server
-//   4) Sleep for WAIT_DELAY
+//   4) Sleep for SLEEP_MS
 const int NUM_READS = 10;
-const int READ_DELAY_MS = 1000;
-const int WAIT_DELAY_MS = 50000;
+const int READ_DELAY_MS = 100;
+// 5 minutes:
+//const int SLEEP_US = 5 * 60 * 1000000;
+const int SLEEP_US = 5 * 1000000;
 
 WiFiClient client;
 void setup()
@@ -90,7 +91,8 @@ void loop()
   
   // Wait in Deep Sleep before repeating the measurement (to save battery)
   // SEE ALSO: https://randomnerdtutorials.com/esp8266-deep-sleep-with-arduino-ide/
-  // NOTE: disable this delay to enable the module to send sensor values every 10 seconds
-  Serial.println("Dropping to Deep Sleep...");  
-  ESP.deepSleep(SLEEP_MS); 
+  // NOTE: Be sure to physically connect GPIO16 to RST or the device will not be
+  //   able to wake itself up.
+  Serial.println("Dropping to Deep Sleep for " + String(SLEEP_US) + " microseconds...");  
+  ESP.deepSleep(SLEEP_US); 
 }
